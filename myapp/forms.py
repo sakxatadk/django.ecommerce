19ef201ea1a589c9798e0aee2ac2d32663a1771f
Sakshyam Adhikari <sakshyam.adhikari@swifttech.com>
2026-02-19 19:29:32 +0545

    Few changes

--- myapp/forms.py
+++ myapp/forms.py
@@ -14,6 +14,14 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields =('username','email','password1','password2')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.widget.attrs.update({
+                'class': 'form-control'
+            })
+            field.help_text = None 
 
 
 
